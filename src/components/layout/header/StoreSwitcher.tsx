874703b4255a892e17c935cc6577fa3795aba408
max jn <maxjn763@gmail.com>
2023-07-17 22:41:43 +0330

    Header & NavBar With Store Switcher: - Added **Header**, **NavBar**, components - Added **Command** and **Popover** components from shadcn/ui - Created **StoreSwitcher** component using Popover and Command components

---
+++ src/components/layout/header/StoreSwitcher.tsx
@@ -0,0 +1,126 @@
+"use client";
+// Hookx / Packages
+import { useState } from "react";
+import { useParams, usePathname, useRouter } from "next/navigation";
+import { useAppDispatch } from "@/redux/hooks";
+import { onOpen } from "@/redux/modal/storeModalSlice";
+
+// Components
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandSeparator,
+  CommandList,
+} from "@/components/ui/command";
+import { cn } from "@/lib/utils";
+
+// Icons
+import {
+  Check,
+  ChevronsUpDown,
+  PlusCircle,
+  Store as StoreIcon,
+} from "lucide-react";
+
+// Types
+import { StoreSwitcherProps } from "@/types/props";
+
+const StoreSwitcher = ({ className, items = [] }: StoreSwitcherProps) => {
+  // Redux
+  const dispatch = useAppDispatch();
+
+  //   Routes
+  const params = useParams();
+  const router = useRouter();
+
+  // States
+
+  const formattedItems = items.map(item => ({
+    label: item.name,
+    value: item.id,
+  }));
+
+  const currentStore = formattedItems.find(
+    item => item.value == params.storeId
+  );
+
+  //   popover state
+  const [popOpen, setPopOpen] = useState(false);
+
+  const onStoreSelect = (store: { value: string; label: string }) => {
+    setPopOpen(false);
+    router.push(`/${store.value}`);
+  };
+
+  return (
+    <Popover open={popOpen} onOpenChange={setPopOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          size={"sm"}
+          role="combobox"
+          aria-expanded={popOpen}
+          aria-label="Select a store"
+          className={cn("w-[200px] justify-between", className)}
+        >
+          <StoreIcon className="me-2 h-4 w-4" />
+          {currentStore?.label}
+          <ChevronsUpDown className="ms-auto h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandList>
+            <CommandInput placeholder="Search store..." />
+            <CommandEmpty>No store found.</CommandEmpty>
+            <CommandGroup heading="Stores">
+              {formattedItems.map(store => (
+                <CommandItem
+                  key={store.value}
+                  onSelect={() => onStoreSelect(store)}
+                  className="text-sm cursor-pointer"
+                >
+                  <StoreIcon className="me-2 h-4 w-4" />
+                  {store.label}
+                  <Check
+                    className={cn(
+                      "ms-auto h-4 w-4",
+                      currentStore?.value === store.value
+                        ? "opacity-100"
+                        : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+          <CommandSeparator />
+          <CommandList>
+            <CommandGroup>
+              <CommandItem
+                onSelect={() => {
+                  setPopOpen(false);
+                  dispatch(onOpen());
+                }}
+              >
+                <PlusCircle className="me-2 h-5 w-5" />
+                Create Store
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default StoreSwitcher;
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -16,12 +16,12 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, name, label, disabled, ...props }, ref) => {
     return (
       <div>
-        <label className="font-medium mb-6">{label}</label>
+        <label className="font-medium ">{label}</label>
         <Field
           type={type}
           name={name}
           className={cn(
-            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 mt-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
             className
           )}
           disabled={disabled}
